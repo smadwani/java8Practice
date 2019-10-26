48a1bc1b227e2a4392fd533fa520846235102bc3
smadwani <sushil.madwani@disneystreaming.com>
2019-10-25 21:43:34 -0400

    master: initial commit

--- src/main/java/madwani/sushil/NthMostOccurringWordInAFile.java
+++ src/main/java/madwani/sushil/NthMostOccurringWordInAFile.java
@@ -13,14 +13,14 @@ public class NthMostOccurringWordInAFile {
     public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
-        int nthFrequesntNumber = sc.nextInt();
+        int nthFrequentNumber = sc.nextInt();
         String [] words = Util.loadFile("sample.txt").split(" ");
         Map<String, Integer> map = Stream.of(words).collect(Collectors.toMap(Function.identity(), e -> e.split(" ").length , (x, y)-> x+y));
         Map<String, Integer> sortedMap = map.entrySet()
                 .stream()
                 .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).collect(
                         Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,(e1,e2)->e1,LinkedHashMap::new));
-        System.out.println(sortedMap.keySet().toArray()[nthFrequesntNumber-1]);
+        System.out.println(sortedMap.keySet().toArray()[nthFrequentNumber-1]);
 
     }
 }

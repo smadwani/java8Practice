b13153f779695563397faedfee9055ad1113235e
smadwani <sushil.madwani@disneystreaming.com>
2019-11-02 18:40:58 -0400

    master: quicksort

--- src/main/java/madwani/sushil/sorting/QuickSort.java
+++ src/main/java/madwani/sushil/sorting/QuickSort.java
@@ -13,57 +13,85 @@ public class QuickSort {
     public static void quicksort(int[] arr, int start, int end) {
 
         // basic check to ensure corner case
+
         if (arr == null || arr.length == 0) {
+
             return;
+
         }
 
         // condition to break recursion
+
         if (start >= end) {
+
             return;
+
         }
 
         // I always chose middle element as Pivot
+
         int middle = start + (end - start) / 2;
 
-        // middle element
+        // Pivot element
+
         int pivot = arr[middle];
 
         // lets start the real sorting
+
         int i = start, j = end;
 
-        // do comparision and swap till start and end have not met
+        // do comparison and swapping till start and end have not met
+
         while (i <= j) {
 
             // increment till left side elements are lesser or equal to pivot
-            while (arr[i] <= pivot) {
+
+            while (arr[i] < pivot) {
+
                 i++;
+
             }
 
             // decrement till right side elements are greater to pivot
+
             while (arr[j] > pivot) {
+
                 j--;
+
             }
 
             // swap elements from left right only when indices haven't crossed each other
+
             // after swap always increase lower index and decrease higher index
-            if (i < j) {
+
+            if (i <= j) {
+
                 Util.swapElementsInArray(arr, i, j);
+
                 i++;
+
                 j--;
+
             }
+
         }
-        // one pass ends, so we have 2 boundaries
-        // one i which was moving towards end
-        // one J which was moving towards start
+
+        // one pass is done, so we have 2 boundaries
+        // lower boundary: i which was moving towards end
+        // upper boundary: j which was moving towards start
+
+
 
         // if i has not reached and crossed the end lets do one more pass from i to end
         // this is left subarray
+
         if (end > i) {
             quicksort(arr, i, end);
         }
 
         // if j has not reached the start and crossed, lets do one more pass from start to j
         // this is right subarray
+
         if (start < j) {
             quicksort(arr, start, j);
         }
